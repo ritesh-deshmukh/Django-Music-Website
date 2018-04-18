09f142c0fc0dffe7f6f37648324d19e6131f3eca
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-04-18 17:40:58 -0500

    Complete with login Multiple versions upcoming

--- website_django/music/views.py
+++ website_django/music/views.py
@@ -1,34 +1,86 @@
-from .models import Album, Song
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Album
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login
+from django.views.generic import View
+from .forms import UserForm
 
 
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def index(request):
-    all_albums = Album.objects.all()
-    return render(request, 'music/index.html', { 'all_albums' : all_albums, })
-
-
-def detail(request, album_id):
-    # instead of album = Album.objects.get(pk = album_id)
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': "You did not select a valid song",
-        })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+    def get_queryset(self):
+        return Album.objects.all()
 
 
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
 
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
 
 
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class ALbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    # display blank form for new user
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # process form data here
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            # cleaned (normalized) data- data that is formatted properly
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns user objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+            return render(request, self.template_name, {'form': form})
+
+# from .models import Album, Song
+# from django.shortcuts import render, get_object_or_404
+#
+#
+#
+# def index(request):
+#     all_albums = Album.objects.all()
+#     return render(request, 'music/index.html', { 'all_albums' : all_albums, })
+#
+#
+# def detail(request, album_id):
+#     # instead of album = Album.objects.get(pk = album_id)
+#     album = get_object_or_404(Album, pk=album_id)
+#     return render(request, 'music/detail.html', {'album': album})
+#
--- website_django/website_django/urls.py
+++ website_django/website_django/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf.urls import include, url
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 # from django.urls import path
 
@@ -9,3 +10,7 @@ urlpatterns = [
     url('music/', include('music.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
